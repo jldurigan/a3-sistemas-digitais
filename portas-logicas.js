a635e2ad0c95ab5355ecb7827d706acc6960e084
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2022-11-25 20:46:59 -0300

    teste js

--- portas-logicas.js
+++ portas-logicas.js
@@ -1,53 +1,56 @@
-let entradaA = false
-let entradaB = true
-function portaNot (entradaA){
-    if(!entradaA){
+var btns = document.querySelectorAll("button");
+
+for(var i = 0; i<btns.length; i++){
+    btns[i].addEventListener("click", function(event){
+        alert("Botão clicado: " + this.innerHTML);
+    })
+}
+
+function portaNot(entradaA) {
+    if (!entradaA) {
         return true
     }
     return false
 }
 
-function portaAnd (entradaA, entradaB){
-    if(entradaA && entradaB){
+function portaAnd(entradaA, entradaB) {
+    if (entradaA && entradaB) {
         return true
     }
     return false
 }
 
-function portaNand (entradaA, entradaB){
-    if(!entradaA && !entradaB || entradaA != entradaB){
+function portaNand(entradaA, entradaB) {
+    if (!entradaA && !entradaB || entradaA != entradaB) {
         return true
     }
     return false
 }
 
-function portaOr (entradaA, entradaB){
-    if(entradaA != entradaB || entradaA && entradaB){
+function portaOr(entradaA, entradaB) {
+    if (entradaA != entradaB || entradaA && entradaB) {
         return true
     }
     return false
 }
 
-function portaNor (entradaA, entradaB){
-    if(!entradaA && !entradaB){
+function portaNor(entradaA, entradaB) {
+    if (!entradaA && !entradaB) {
         return true
     }
     return false
 }
 
-function portaXor (entradaA, entradaB){
-    if(entradaA && !entradaB || !entradaA && entradaB){
+function portaXor(entradaA, entradaB) {
+    if (entradaA && !entradaB || !entradaA && entradaB) {
         return true
     }
     return false
 }
 
-function portaNxor (entradaA, entradaB){
-    if(entradaA && entradaB || !entradaA && !entradaB){
+function portaNxor(entradaA, entradaB) {
+    if (entradaA && entradaB || !entradaA && !entradaB) {
         return true
     }
     return false
 }
-
-
-console.log(portaNxor(entradaA,entradaB))
