9857751577ca8d1f1f9d9bfa5f5675955282c68c
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2022-11-25 22:17:34 -0300

    merge

--- portas-logicas.js
+++ portas-logicas.js
@@ -1,44 +1,44 @@
 var btns = document.querySelectorAll("button");
 
-for(var i = 0; i<btns.length; i++){
-    btns[i].addEventListener("click", function(event){
-        this.value = this.value=="false";
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function (event) {
+        
+        if (this.value == "false" || this.value == "null") {
+            this.textContent = "On";
+            this.style.backgroundColor = "green";
+            this.value = true;
+        } else {
+            this.textContent = "Off";
+            this.style.backgroundColor = "red";
+            this.value = false;
+        }
+
         var tipoPorta = this.name;
         var portas = document.getElementsByName(tipoPorta)
-        
-        switch(tipoPorta){
+
+        switch (tipoPorta) {
             case 'portaNot':
                 alert(portaNot(this.value));
                 break;
-                case 'portaAnd':
-                    alert(portaAnd(portas[0].value, portas[1].value));
-                    break;
-                    case 'portaNand':
-                        alert(portaNand(portas[0].value, portas[1].value));
-                        break;
-                        case 'portaOr':
-                            alert(portaOr(portas[0].value, portas[1].value));
-                            break;
-                            case 'portaNor':
-                                alert(portaNor(portas[0].value, portas[1].value));
-                                break;
-                                case 'portaXor':
-                                    alert(portaXor(portas[0].value, portas[1].value));
-                                    break;
-                                    case 'portaNxor':
-                                        alert(portaNxor(portas[0].value, portas[1].value));
-                                        break;
+            case 'portaAnd':
+                alert(portaAnd(portas[0].value, portas[1].value));
+                break;
+            case 'portaNand':
+                alert(portaNand(portas[0].value, portas[1].value));
+                break;
+            case 'portaOr':
+                alert(portaOr(portas[0].value, portas[1].value));
+                break;
+            case 'portaNor':
+                alert(portaNor(portas[0].value, portas[1].value));
+                break;
+            case 'portaXor':
+                alert(portaXor(portas[0].value, portas[1].value));
+                break;
+            case 'portaNxor':
+                alert(portaNxor(portas[0].value, portas[1].value));
+                break;
         }
-            if(this.textContent == "Off" || this.textContent == "Vazio"){
-                this.textContent = "On";
-                this.style.backgroundColor = "green";
-                this.value = true;
-            }else{
-                this.textContent = "Off";
-                this.style.backgroundColor = "red";
-                this.value = false;
-            }
-        alert("Botão clicado: " + this.innerHTML + " " + this.value);
     })
 }
 
